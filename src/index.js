9e85336f0c76a57519861b537ede648e15beb610
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-02 17:07:07 -0600

    setting up the action creator function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import catsReducer from './reducers/catsReducer.js';
 import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer.js';
 
