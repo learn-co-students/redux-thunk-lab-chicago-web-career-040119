3c67ff242609711ee14159a0c574795c135d0323
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-02 16:51:36 -0600

    completed redux configuration, used Redux's createStore() function to initialize store in index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import catsReducer from './reducers/catsReducer.js';
 
+const store = createStore(catsReducer);
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')  
 );
