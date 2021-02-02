9aafd46d4353074ce5f50aaccf4da51596c8eeb3
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-02 16:55:10 -0600

    implemeted thunk by passing thunk into applyMiddleware as a second prop to createStore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import catsReducer from './reducers/catsReducer.js';
+import thunk from 'redux-thunk';
+import catsReducer from './reducers/catsReducer.js';
 
-const store = createStore(catsReducer);
+const store = createStore(catsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
