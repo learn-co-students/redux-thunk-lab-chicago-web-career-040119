c69b347865816703d0dc83b1d643bb2f1ce686bd
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-02-02 17:29:57 -0600

    completed catList component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        <CatsList />
       </div>
     );
   }
